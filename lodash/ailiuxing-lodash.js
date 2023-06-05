5206e3cf3cb21a275c96d993bba9da1e819e0892
fvdfdv <fvdfdv@qq.com>
2023-06-05 10:42:00 +0800

    1

---
+++ lodash/ailiuxing-lodash.js
@@ -0,0 +1,9 @@
+var ailiuxing = {
+  chunk: function (ary, size) {
+    var result = []
+    for (var i = 0; i < ary.length; i += size) {
+      result.push(ary.slice(i, i + size))
+    }
+    return result
+  }
+}
