3c19c44a0e965016f1c2f2a5de286a46cb165a88
fvdfdv <fvdfdv@qq.com>
2023-06-05 10:47:35 +0800

    123

--- lodash/ailiuxing-lodash.js
+++ lodash/ailiuxing-lodash.js
@@ -7,3 +7,21 @@ var ailiuxing = {
     return result
   }
 }
+
+
+var ailiuxing = {
+  function compact(array) {
+    var index = -1,
+    length = array == null ? 0 : array.length,
+    resIndex = 0,
+    result = [];
+
+while (++index < length) {
+  var value = array[index];
+  if (value) {
+    result[resIndex++] = value;
+  }
+}
+return result;
+  }  
+}
