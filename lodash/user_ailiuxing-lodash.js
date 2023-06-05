5bedda410e7a343d1bf0b7b12e97c930fbb74786
fvdfdv <fvdfdv@qq.com>
2023-06-05 10:37:31 +0800

    12

--- lodash/user_ailiuxing-lodash.js
+++ lodash/user_ailiuxing-lodash.js
@@ -1,9 +1,9 @@
 var user_ailiuxing = {
   chunk: function (ary, size) {
-    let res = []
-    for (let i = 0; i < ary.length; i += size) {
-      res.push(ary.slice(i, i + size))
+    var result = []
+    for (var i = 0; i < ary.length; i += size) {
+      result.push(ary.slice(i, i + size))
     }
-    return res
+    return result
   }
 }
